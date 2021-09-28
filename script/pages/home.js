90448fe0f6111191fc277b2e9547f63e4cb65cf1
Vinicius Gustavo <vgustavobzr@gmail.com>
2021-09-28 16:52:34 -0300

    Fix tablet versions

--- script/pages/home.js
+++ script/pages/home.js
@@ -1,41 +1,15 @@
 export default class Home {
 
-  loginCache(){
-    $(document).on('click', '.prateleira a.btn_login', function(){
-      console.log(window.location.href)
-      sessionStorage.setItem("urlatual", window.location.href);
-    })
-    $(document).on('click','.select_buy a.btn_login', function(){
-      console.log(window.location.href)
-      sessionStorage.setItem("urlatual", window.location.href);
-    })
-  }
+  testeFunction(){
 
-  logadoHome(){
-    $.ajax({
-      url: '/api/vtexid/pub/authenticated/user',
-      type: 'GET'
-    }).done(function (user) {
-      console.log(user)
-    if (user === null || user.user === 'suporte@sandersdigital.com.br') {
-     } else {
-      if (sessionStorage.getItem("urlatual")) {
-        var url = sessionStorage.getItem("urlatual");
-        window.location.href = url; 
-        sessionStorage.removeItem('urlatual');
-      }
-     }
-    });
   }
 
   init(){
     let that = this;
-    console.log("teste");
-    that.loginCache();
+
     if($('body').hasClass('home')){
-      if (sessionStorage.getItem("urlatual")) {
-        that.logadoHome();
-      }else{}
+
+      that.testeFunction();
     }
   }
 }
